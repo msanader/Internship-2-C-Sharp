904e522a3914e0daa8dcca87a53ea875bc033a29
msanader <mirna.sanader@outlook.com>
2021-11-18 20:32:37 +0100

    Stvoren program.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Internship_2_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            };
+        }
+    }
+}
